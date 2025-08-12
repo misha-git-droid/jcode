f5a01cf0b23c61ee5192d2b202aedb4dc3131975
misha-git-droid <budarin.flip@gmail.com>
2025-08-13 00:39:08 +0300

    feat(core): adding basic functionality

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,35 @@
 package com;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0),
+                new Order("Watch", 200)
+        );
+
+        orders.stream()
+                .collect(Collectors.groupingBy(Order::getProduct))
+                .entrySet().stream().map(entry -> {
+                  String product = entry.getKey();
+                  double totalCost = entry.getValue().stream()
+                          .mapToDouble(Order::getCost)
+                          .sum();
+                  return Map.entry(product, totalCost);
+                })
+                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                .limit(3)
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+
+
+
 
     }
 }
