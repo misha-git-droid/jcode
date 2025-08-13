f3447daedcf3d3b6ca1a4f4d95e409a46c9a0a70
misha-git-droid <budarin.flip@gmail.com>
2025-08-13 11:14:51 +0300

    feat(core): adding basic functionality

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,17 @@
 package com;
 
+import java.util.concurrent.ForkJoinPool;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 5;
 
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = (long) forkJoinPool.invoke(factorialTask);
+
+
+        System.out.println("Факториал " + n + "! = " + result);
     }
 }
